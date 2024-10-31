e8aeda992ba0a2ec9252e671f36158eb4aac96dc
Seb Portable <sebastien.giguet@ynov.com>
2024-10-31 15:22:35 +0100

    Fix: folder_Vendor

--- .platform/hooks/postdeploy/postdeploy.sh
+++ .platform/hooks/postdeploy/postdeploy.sh
@@ -2,23 +2,23 @@
 echo "Postdeploy script"
 
 echo "Création des dossiers dans /var/current"
-mkdir -p /var/current/storage/framework/sessions
-mkdir -p /var/current/storage/framework/views
-mkdir -p /var/current/storage/framework/cache
-mkdir -p /var/current/storage/logs
+mkdir -p /var/app/current/storage/framework/sessions
+mkdir -p /var/app/current/storage/framework/views
+mkdir -p /var/app/current/storage/framework/cache
+mkdir -p /var/app/current/storage/logs
 
 echo "Changement des permissions des dossiers dans /var/current"
-chmod -R 777 /var/current/storage 
-chmod -R 777 /var/current/storage/framework
-chmod -R 777 /var/current/storage/framework/sessions
-chmod -R 777 /var/current/storage/framework/views
-chmod -R 777 /var/current/storage/framework/cache
-chmod -R 777 /var/current/storage/logs
-chmod -R 777 /var/current/resources/lang/
-chmod -R 777 /var/current/bootstrap/cache/
+chmod -R 777 /var/app/current/storage 
+chmod -R 777 /var/app/current/storage/framework
+chmod -R 777 /var/app/current/storage/framework/sessions
+chmod -R 777 /var/app/current/storage/framework/views
+chmod -R 777 /var/app/current/storage/framework/cache
+chmod -R 777 /var/app/current/storage/logs
+chmod -R 777 /var/app/current/resources/lang/
+chmod -R 777 /var/app/current/bootstrap/cache/
 
 echo "Changement droit utilisateurs et groupes des dossiers dans /var/current"
-chown -R root:root /var/current/storage
+chown -R root:root /var/app/current/storage
 
 echo "Création des dossiers dans /var/www/html"
 mkdir -p /var/www/html/storage/framework/sessions

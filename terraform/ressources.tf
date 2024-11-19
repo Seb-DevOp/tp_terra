744a24d7315c10e3f94cbff1fed789df7a58e21a
Seb Portable <sebastien.giguet@ynov.com>
2024-11-19 16:06:08 +0100

    first

--- terraform/ressources.tf
+++ terraform/ressources.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "laravel_instance" {
   ami           = "ami-0864890e218d4dd30"
   instance_type = "t2.micro"
-  key_name      = "my-key-pair"  # Assurez-vous que votre clé SSH est présente dans AWS
   security_groups = ["sg-06a9de32b71ebd17d - awseb-e-y3gwgpj9t5-stack-AWSEBSecurityGroup-ojDaqxNIFPYL"] 
   tags = {
     Name = "seb_terra_ec2"

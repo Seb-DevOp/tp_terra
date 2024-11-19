e91301d61fc9319313922fa0b0c964268b38b960
Seb Portable <sebastien.giguet@ynov.com>
2024-11-19 16:12:12 +0100

    first

--- terraform/ressources.tf
+++ terraform/ressources.tf
@@ -1,7 +1,8 @@
 resource "aws_instance" "laravel_instance" {
   ami           = "ami-0864890e218d4dd30"
   instance_type = "t2.micro"
-  security_groups = ["sg-06a9de32b71ebd17d - awseb-e-y3gwgpj9t5-stack-AWSEBSecurityGroup-ojDaqxNIFPYL"] 
+  key_name = "Terra_seb"
+  security_groups = ["sg-06a9de32b71ebd17d"] 
   tags = {
     Name = "seb_terra_ec2"
   }
